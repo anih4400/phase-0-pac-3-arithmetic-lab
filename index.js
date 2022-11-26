9992b1b17274614dde6764d1e6cf612c8138d1a3
AnishPatel <anish4400@gmail.com>
2022-11-26 18:46:39 -0500

    commit tree aee2fbc2345a401bfe43dd0ca421ba99f6c95504

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+  }
+
+  function subtract() {
+    // we'll fill this in shortly
+  }
+
+  function multiply() {
+    // we'll fill this in shortly
+  }
+
+  function divide() {
+    // we'll fill this in shortly
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+
+  function increment(n){
+    return n += 1 //increment(n) increments n and returns the result
+  }
+  function decrement(n){
+    return n -= 1 //decrement(n) decrements n and returns the result
+  }
+
+  function makeInt(n){
+    return parseInt(n, 10) //parses n as an integer and returns the parsed integer
+
+  } //returns NaN as appropriate
+
+  function preserveDecimal(n){
+    return parseFloat(n) //returns NaN as appropriate
+  }
+
+
+
+ 
+  
